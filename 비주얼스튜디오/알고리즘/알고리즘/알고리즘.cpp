902ac7025d6a28fe189495a383e65bd0d7357078
sungmin-Q <smn1196@naver.com>
2023-01-17 14:12:57 +0900

    0117

---
+++ 비주얼스튜디오/알고리즘/알고리즘/알고리즘.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	a = a + 1;
+	cout << b - a << ' ' <<endl;
+	for (int i = a; i < b; i++)
+		cout << i << ' ';
+}
