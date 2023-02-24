36470512192892d59c5ece36dcfff6ffb74e9306
sungmin-Q <smn1196@naver.com>
2023-02-24 15:55:16 +0900

    0224

--- mincoding/mincoding/mincoding.cpp
+++ mincoding/mincoding/mincoding.cpp
@@ -2115,6 +2115,8 @@ int main() {
 }
 */
 
+/*
+문자만들기
 #include <iostream>
 #include <algorithm>
 #include <cstring>
@@ -2133,8 +2135,8 @@ void dfs(int lev, int result) {
 		return;
 	}
 	int backup[4];
-	//backup 에다가 처음 시작 상태 저장
-	memcpy(backup, leftover, 4*4);
+	memcpy(backup, leftover, 4*4);	//backup 에다가 처음 시작 상태 저장
+
 
 	for (int i = 0; i < 4; i++) {
 		if (leftover[i] == 0) continue;
@@ -2171,4 +2173,466 @@ int main() {
 		cout << "#" << T << " " << Max - Min << "\n";
 	}
 	return 0;
+}
+*/
+
+
+/*
+//#미생물
+#include <iostream>
+using namespace std;
+struct Node {
+	int row;
+	int col;
+	int num;
+	int dir;
+};
+void move(Node node) {
+	if (node.dir == 1) {
+		node.row += 1;
+	}
+	else if (node.dir == 2) {
+		node.row -= 1;
+	}
+	else if (node.dir == 2) {
+		node.row -= 1;
+	}
+	else if (node.dir == 2) {
+		node.row -= 1;
+	}
+}
+
+int N, M, K;
+Node node[9];
+void input() {
+	cin >> N >> M >> K;
+	for (int i = 0; i < K; i++)
+		cin >> node[i].row >> node[i].col >> node[i].num >> node[i].dir;
+}
+int main() {
+	memset(node, 0, sizeof(Node)*9);
+	input();
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < K; j++) {
+			node[k]
+		}
+	}
+}
+*/
+
+/*
+#탈주범
+플러드필 (연결을 확인)
+주어진 시간만큼 count
+1.다음 파이프로 갈수 있는지 체크
+2.L만큼의 시간동안 퍼짐
+	->단게별 floodfill
+
+
+상하좌우 방향바꾸기
+1. {0,1,2,3}
+상->하 +1
+하->상 -1
+
+*/
+
+/*
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int pipedir[][4] = {
+	{0,0,0,0},
+	{1,1,1,1},
+	{1,1,0,0},
+	{0,0,1,1},
+	{1,0,0,1},
+	{0,1,0,1},
+	{0,1,1,0},
+	{1,0,1,0}
+};
+int N, M, R,C,L;
+int map[50][50];
+struct Node {
+	int row;
+	int col;
+};
+void bfs(int row, int col) {
+	queue<Node> que;
+	que.push({ row,col });
+
+	int visited[50][50] = { 0 };
+	visited[row][col] = 1;
+
+	int dr[] = { 1,-1,0,0 };
+	int dr[] = { 1,-1,0,0 };
+	int time = 0;
+	int cnt = 0; //방문 파이프개수
+
+	while (!que.empty()) {
+		//여기 돌아오면 한시간이 지남(단계의 완료)
+		time++;
+		
+		//L의 시간 만큼 지낸경우 종료
+		if (time == L)
+			break;
+		Node now = que.front();
+		que.pop();
+		//now -> 진입한 파이프
+		cnt++;
+		//단계별 -> 지금 que에 들어있는 만큼들 들어가도록
+		int cursize = que.size();
+		int dr[] = { 1,-1,0,0 };
+		int dc[] = { 0,0,-1,1 };
+		for (int i = 0; i < cursize; i++) {
+
+			for (int i = 0; i < 4; i++) {
+				int nr = now.row + dr[i];
+				int nc = now.col + dc[i];
+				if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+				if (visited[nr][nc] == 1) continue;
+				//갈 수 있는지를 판단
+				int curpipe = map[now.row][now.col];
+				// 1.지금의 파이프에서 방향이동 불가
+				if (pipedir[curpipe][i] == 0) continue;
+				// 2.벽 이동 불가
+				if (map[nr][nc] == 0) continue;
+				// 3.다음 파이프가 방향에서 왔을때 들어갈수 있는지
+				// 옆에 있는 파이프가 열려있는지 확인
+				int nextpipe = map[nr][nc];
+				//다음 파이프가 지금 파이프에서 가는 방향의 반대로 open
+				int nextdir = -1;
+				if (i % 2 == 0) nextdir = i + 1;
+				else nextdir = i - 1;
+				if (pipedir[nextpipe][nextdir] == 0) continue;
+
+				visited[nr][nc] = 1;
+				que.push({ nr,nc });
+			}
+		}
+	}
+}
+int main() {
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		memset(map, 0, sizeof(map));
+		cin >> N >> M >> R >> C >> L;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				cin >> map[i][j];
+			}
+		}
+	}
+}
+*/
+
+/*
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, M, R, C, L;
+int arr[50][50];
+
+struct Node {
+	int row;
+	int col;
+};
+int pipedir[][4] = {
+	// 상하좌우
+	// 갈 수있으면 1, 아니면 0
+	{0, 0, 0, 0},
+	{1, 1, 1, 1}, // +
+	{1, 1, 0, 0},
+	{0, 0, 1, 1},
+	{1, 0, 0,1 },
+	{0, 1, 0, 1},
+	{0, 1, 1, 0},
+	{1, 0, 1, 0}
+};
+int bfs(int row,int col) {
+	queue<Node> que;
+	que.push({ row,col });
+
+	int visited[50][50] = { 0 };
+	visited[row][col] = 1;
+
+	int dr[] = { 1,-1,0,0 };
+	int dc[] = { 0,0,-1,1 };
+
+	int time = 0;
+	int cnt = 0;
+
+	while (!que.empty()) {
+		
+		if (time == L) break;
+		time++;
+		int cursize = que.size();
+		for (int j = 0; j < cursize; j++) {
+			Node now = que.front();
+			que.pop();
+			cnt++;
+
+			for (int i = 0; i < 4; i++) {
+				
+				int nr = now.row + dr[i];
+				int nc = now.col + dc[i];
+				if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+				if (visited[nr][nc] == 1) continue;
+
+				int curpipe = arr[now.row][now.col];
+				if (pipedir[curpipe][i] == 0) continue;
+				
+				if (arr[nr][nc] == 0) continue;
+
+				int nextpipe = arr[nr][nc];
+				int nextdir = -1;
+				if (i % 2 == 0)
+					nextdir = i + 1;
+				else
+					nextdir = i - 1;
+				if (pipedir[nextpipe][nextdir] == 0) continue;
+				
+				visited[nr][nc] = 1;
+				que.push({ nr,nc });
+			}
+		}
+	}
+	return cnt;
+}
+int main() {
+	freopen_s(new FILE*, "input.txt", "r", stdin);
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		memset(arr, 0, sizeof(arr));
+		cin >> N >> M >> R >> C >> L;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				cin >> arr[i][j];
+			}
+		}
+
+		int ans = bfs(R, C);
+
+		cout << '#' << tc << ' ' << ans << '\n';
+	}
+}
+*/
+/*
+#핀볼 
+외부 선 생성해줌
+*/
+
+/*
+#include <iostream>
+using namespace std;
+struct coord {
+	int row;
+	int col;
+};
+struct Ball {
+	int row;
+	int col;
+	int dirr;
+	int dirc;
+};
+int arr[6][6];
+Ball ball;
+int N;
+void input(){
+	cin >> ball.row >> ball.col;
+	N = 4;
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (i == 0 || j == 0 || j == 5 || j == 5) {
+				arr[i][j] = 5;
+				continue;
+			}
+			cin >> arr[i][j];
+			//윔홀의 저장
+			if (arr[i][j] >= 6 && arr[i][j] <= 9)
+				hole[i] = { i,j };
+		}
+	}
+
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			cout << arr[i][j];
+		}
+		cout << endl;
+	}
+}
+int dfs(Ball ball) {
+	
+	int dr[] = { 1,-1,0,0 };
+	int dc[] = { 0,0,-1,1 };
+	for (int i = 0; i < 4; i++) {
+		int nr = ball.row + dr[i];
+		int nc = ball.col + dc[i];
+		if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+		if (arr[nr][nc] == 0) {
+			ball.row = nr;
+			ball.col = nc;
+			dfs(ball);
+		}
+		else if (arr[nr][nc] == 1) {
+			ball.row = nr;
+			ball.col = nc;
+			ball.dirr
+			dfs(ball);
+		}
+	}
+}
+coord hole[4];
+int main() {
+	freopen_s(new FILE*, "input.txt", "r", stdin);
+	input();
+	ball.dirr = 1, ball.dirc = 0;
+	int a = go(ball);
+}
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N; // 세로 크기
+int M; // 가로 크기
+int R; // 맨홀의 세로 위치 (y) 
+int C; // 맨홀의 가로 위치 
+int L; // 소요시간
+int MAP[50][50];
+
+// 파이프가 연결하는 방향에 대한 정보를 담는 dir
+int pipedir[][4] = {
+	// 상하좌우
+	// 갈 수있으면 1, 아니면 0
+	{0, 0, 0, 0},
+	{1, 1, 1, 1}, // +
+	{1, 1, 0, 0},
+	{0, 0, 1, 1},
+	{1, 0, 0,1 },
+	{0, 1, 0, 1},
+	{0, 1, 1, 0},
+	{1, 0, 1, 0}
+};
+
+struct Node {
+	int y;
+	int x;
+};
+
+// 무지성 flood fill 
+// 이제 추가해야 되는 조건 
+// #1. 파이프 
+// --> 내가 지금 파이프에서 다음 파이프로 갈수 있는가? 
+// #2. L만큼의 시간동안 퍼진다. 
+// --> 단계별 flood fill
+
+int bfs(int y, int x) {
+	// queue 준비
+	queue<Node>q;
+	q.push({ y, x });
+
+	// visited 준비
+	int visited[50][50] = { 0, };
+	visited[y][x] = 1;
+
+	// 방향 배열
+	int ydir[] = { -1, 1, 0, 0 };
+	int xdir[] = { 0, 0, -1, 1 };
+
+	int time = 0;
+	int cnt = 0; // 내가 방문한 파이프의 개수
+
+	while (!q.empty()) {
+		// 어느 순간 내가 L의 시간 만큼 지낸 때가 온다. 
+		if (time == L)
+			break;
+
+		// 여기 돌아오면 -> 하나의 단계가 완료
+		// -> 1시간이 지났다 
+		time++;
+
+		// 일단은 단계별로 -> 지금 queue에 들어있는 만큼만
+		// 돌아갈 수 있도록 구성
+
+		int cursize = q.size();
+		for (int j = 0; j < cursize; j++) {
+
+			// now -> 내가 지금 진입한 파이프 
+			// now = 지금 내가 갈 수 있는 파이프
+			// now가 나왔다는 것 = 내가 갈수 있는 파이프가 하나 늘었다!
+			Node now = q.front();
+			q.pop();
+
+			cnt++;
+
+			for (int i = 0; i < 4; i++) {
+				int ny = now.y + ydir[i];
+				int nx = now.x + xdir[i];
+				if (ny < 0 || nx < 0 || ny >= N || nx >= M)
+					continue;
+				if (visited[ny][nx] == 1)
+					continue;
+
+				// 갈수 있는지 판단
+				// #1. 지금 내 파이프가 i번 방향으로 갈수있는가? 
+				int curpipe = MAP[now.y][now.x]; // 지금 내 파이프 번호 
+				// 만약 지금 내 파이프에서 i번째 방향으로 못간다면 continue
+				if (pipedir[curpipe][i] == 0)
+					continue;
+
+				// #2. 벽이면 못간다
+				if (MAP[ny][nx] == 0)
+					continue;
+
+				// #3. 지금 내가 (ny, nx) 파이프로 가려고하는데
+				// 여기까지 왔다는건 다음 위치가 벽도 아니고
+				// 지금 나는 이 방향으로 갈 수 있다 
+				// --> 다음 파이프가 내가 지금 행하는 방향에서
+				// 왔을때 -> 들어갈수 있는가? 
+				int nextpipe = MAP[ny][nx];
+				// 다음 파이프가 지금 파이프에서 가는 방향의
+				// 반대방향이 뚫려있는가? 
+				// 내가 지금 가는 i방향의 반대방향을 확인
+				int nextdir = -1;
+				if (i % 2 == 0)
+					nextdir = i + 1;
+				else
+					nextdir = i - 1;
+				if (pipedir[nextpipe][nextdir] == 0)
+					continue;
+
+				visited[ny][nx] = 1;
+				q.push({ ny, nx });
+			}
+		}
+	}
+	return cnt;
+}
+
+int main() {
+
+	freopen("input.txt", "r", stdin);
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		// reset
+		memset(MAP, 0, sizeof(MAP));
+
+		// input
+		cin >> N >> M >> R >> C >> L;
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				cin >> MAP[i][j];
+
+		// solve
+		int ans = bfs(R, C);
+
+		// output
+		cout << "#" << tc << " " << ans << '\n';
+	}
 }
